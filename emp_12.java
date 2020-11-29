1ae966838fae6c21f3f67b36c83468ef77b0ce0b
Prasanna-Nanda <prasannanandakumar92@gmail.com>
2020-11-29 21:25:56 +0530

    Added emp

---
+++ emp_12.java
@@ -0,0 +1,16 @@
+import java.util.Random;
+import java.lang.*;
+import java.io.*;
+
+
+class Employee
+{
+
+        public static void main(String[] args)
+        {
+            int head=1;
+            int tail=0;
+
+            System.out.println("Welcome to employee wage computation");
+	}
+}
