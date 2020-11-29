651c7bfd119f6d477ca04914fb672a3058296ea7
Prasanna-Nanda <prasannanandakumar92@gmail.com>
2020-11-29 21:29:36 +0530

    Added attend

--- emp_12.java
+++ emp_12.java
@@ -12,5 +12,17 @@ class Employee
             int tail=0;
 
             System.out.println("Welcome to employee wage computation");
+
+
+Random random=new Random();
+            int  randomNum=random.nextInt(2);
+            if(randomNum == head)
+            {
+                System.out.println("Employee is present:"+randomNum);
+            }
+            else
+            {
+                System.out.println("Employee is absent:"+randomNum);
+            }
 	}
 }
